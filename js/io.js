0e618b3f1f16a8061472fc29323a934df6d345c0
KaitoHH <hh.kaito@gmail.com>
2017-08-04 11:22:38 +0800

    feat: add upload mode

--- js/io.js
+++ js/io.js
@@ -1,9 +1,46 @@
 'use strict';
 var submitBtn = document.getElementById('toGray');
 var text = document.getElementsByClassName('result')[0];
+var uploadMode = document.getElementById('uploadmode');
 
-function upload() {
-    text.innerText = 'waiting...';
+function handleClick() {
+    var gray = getPixel();
+    if (uploadMode.checked) {
+        var label = prompt("What is it?");
+        var num1 = parseInt(label);
+        console.log(num1);
+        if (num1 >= 0 && num1 <= 9) {
+            upload(gray, num1);
+        } else {
+            alert('This is not a vaild number [0-9]');
+        }
+    } else {
+        show(gray);
+    }
+}
+
+function upload(gray, label) {
+    text.innerText = 'uploading...';
+    gray["label"] = label;
+    $.ajax({
+        type: 'POST',
+        url: 'https://dbi342070trial.hanatrial.ondemand.com/mnist/api/upload',
+        headers: {
+            'Authorization': 'Basic R09EVVNFUjpLYWl0b2hoa2FpdG9oaDExMQ==',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        crossDomain: true,
+        data: JSON.stringify(gray),
+        success: function(data) {
+            console.log(data);
+            text.innerText = 'upload success!';
+            document.getElementById('clear').click();
+        }
+    });
+}
+
+function getPixel() {
     var rgba = ctx.getImageData(0, 0, painter.width, painter.width).data;
     var gray = {
         "id": 1
@@ -14,6 +51,12 @@ function upload() {
         gray["p" + i / 4] = 255 - rgba[i];
     }
     // console.log(gray);
+    return gray;
+
+}
+
+function show(gray) {
+    text.innerText = 'waiting...';
 
     var showResult = function() {
         text.innerText = 'nearly done...';
@@ -45,9 +88,9 @@ function upload() {
     });
 }
 
-submitBtn.addEventListener('click', upload);
+submitBtn.addEventListener('click', handleClick);
 document.addEventListener('keypress', function(e) {
     if (e.keyCode == 13) {
-        upload();
+        handleClick();
     }
 });
